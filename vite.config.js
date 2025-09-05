2110620bc2e993b2e4835ddf88520462c3f2f73e
CastroFR <kevinfrancisco906@gmail.com>
2025-09-05 16:08:15 -0600

    Fix: Corrigiendo errores de vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Asegura que el directorio de salida sea 'dist'
+    chunkSizeWarningLimit: 1000, // Aumenta el límite de advertencia de tamaño
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'firebase/app', 'firebase/firestore', 'firebase/auth']
+        }
+      }
+    }
+  }
 })
